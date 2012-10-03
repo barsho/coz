33c0363a5eb49db1693800adb4cd9f317c2944fd
Serhan Ozgul <serhanozgul@gmail.com>
2012-10-02 21:48:57 -0700

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Coz::Application.routes.draw do
+  resources :users
 
   root :to => 'static_pages#splash'
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,7 @@ require 'spec_helper'
 describe User do
 
   before { @user = User.new(name: "Example User", email: "user@example.com", 
-                            password: "foobar", password_confirmation: "foobar") }
+                            password: "foobar") }
 
   subject { @user }
 
