92d216e70d1f5b007f56a3d36abe7a7f8f779e75
Serhan Ozgul <serhanozgul@gmail.com>
2012-10-06 23:46:24 -0700

    finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,8 @@ class User < ActiveRecord::Base
   has_secure_password  
   
   before_save { |user| user.email = email.downcase }
-  
+  before_save :create_remember_token
+    
   validates :firstname, presence: true, length: { maximum: 50 }
   validates :lastname, presence: true, length: { maximum: 50 }
 
@@ -23,5 +24,11 @@ class User < ActiveRecord::Base
                        uniqueness: { case_sensitive: false }
 
   validates :password, presence: true, length: { minimum: 6 }
-  
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
+    
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,13 @@
-def full_title(page_title)                          # Method definition
-  base_title = "coz"  # Variable assignment
-  if page_title.empty?                              # Boolean test
-    base_title                                      # Implicit return
-  else
-    "#{page_title}.#{base_title}"                 # String interpolation
+include ApplicationHelper
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
   end
 end
